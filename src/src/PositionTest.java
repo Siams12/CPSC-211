aeb4cd4836cf855d0b2f12d2a90fcbe99dc6648b
Siams12 <82103303+Siams12@users.noreply.github.com>
2021-09-26 12:41:42 -0400

    Add files via upload

---
+++ src/src/PositionTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PositionTest {
+
+	@Test
+	public void testPosition() {
+		Maze maze = new Maze(5,5);
+		fail("Not yet implemented");
+	}
+
+}
